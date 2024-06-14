50072662c29214b72eda223cca83e3e12292858c
Mosadoluwa-A <magbonyin@gmail.com>
2024-06-14 05:56:46 +0100

    First Commit

---
+++ src/main/java/com/poc/address/book/entity/Address.java
@@ -0,0 +1,19 @@
+package com.poc.address.book.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String phonenumber;
+
+
+}
+
+
